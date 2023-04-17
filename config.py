c7e28141b534293bec0a7b464f93182df280e4c9
AlexanderCherniy <99901042+AlexanderCherniy@users.noreply.github.com>
2023-04-17 17:32:15 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+#By кот хакер 1234#
+#Downloaded from TG @top_softs
+#Leave this inscription#
+
+token='6159786619:AAEOlNulscMMDK2KR6BtuAwQl8i7PkZTTW4' #https://t.me/BotFather 
+admin_id=int(5889401024) #https://t.me/getidsbot 
+
